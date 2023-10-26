4cdf5f9a555b59c686443d2c67c4647869ecaee0
Geffrey van der Bos <geffrey.bos@gmail.com>
2023-10-26 19:42:18 +0200

    cleaned it up a bit, added a webmention link

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
-// const { DateTime } = require("luxon");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     // eleventyConfig.addPassthroughCopy('./src/notes');
     
-    // eleventyConfig.addFilter("postDate", (dateObj) => {
-    //     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    // })
+    eleventyConfig.addFilter("asPostDate", (dateObj) => {
+        // return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+        return DateTime.fromJSDate(dateObj).toFormat('yyyy-MM-dd HH:mm');
+    })
 
     return {
         dir: {
--- src/notes/this-is-a-though copy.md
+++ src/notes/this-is-a-though copy.md
@@ -1,8 +1,7 @@
 ---
-title: This is my first thought on Eleventy.
+title: Use Obsidian and Working Copy as CMS
 author: Geffrey van der Bos
-date: 2021-06-02
 tags: permanentNotes
 ---
 
-This is a thought I hold dearly. One day I will be syndicating this to Mastodon. And people will be able to reply to this message there, and it would end up here. Mark my words.
+But that would mean I'd have to make sure everything is up and running, first.
