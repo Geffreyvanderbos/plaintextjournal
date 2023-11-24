86304e8430bf7722493271c22b100225cee8dd62
Geffrey van der Bos <geffrey.bos@gmail.com>
2023-11-24 20:43:49 +0100

    Added README.md, TODO.md, and RSS on /feed.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 
 module.exports = function(eleventyConfig) {
 
@@ -10,13 +12,9 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     
-    eleventyConfig.addFilter("asPostDate", (dateObj) => {
-        // return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-        return DateTime.fromJSDate(dateObj).toFormat('yyyy-MM-dd HH:mm');
-    })
-    eleventyConfig.addFilter("toISO8601", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toISO();
-    })
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateToRfc3339);
+    eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
 
     return {
         dir: {
---
+++ TODO.md
@@ -0,0 +1,18 @@
+## Journal
+
+- [ ] Rewrite existing articles with the focus on clarity and flow
+- [ ] Add authors and contributors to articles
+- [ ] Clean up design on mobile
+- [ ] Rewrite the mission statement and introduction copy on site
+- [ ] Write the guiding principles of the project
+- [ ] Add back dark mode
+- [ ] Add categories
+- [ ] Add a public changelog
+- [ ] Introduction in the aside should include links to get started
+- [ ] Fix Joplin paragraph in Reading and Writing Plain Text (Tools)
+- [ ] Add ReText for Linux in Reading and Writing Plain Text (Tools) (Thank you Anita Lewis)
+- [ ] Make RSS feed more clear, if functional at all
+- [ ] Add a proper 404 page.
+
+## Article Ideas
+- [ ] Moving from proprietary systems to Plaintext
